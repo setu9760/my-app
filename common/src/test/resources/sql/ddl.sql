8291487ab4b443d452081a98111797b771af6b45
desai@BRIWS0609.sst.stp <setu_desai02@yahoo.com>
2016-03-23 13:52:08 +0000

    init commit

---
+++ common/src/test/resources/sql/ddl.sql
@@ -0,0 +1,73 @@
+
+CREATE TABLE `scholorship` (
+  `id` varchar(36) NOT NULL,
+  `external_ref` varchar(45) NOT NULL DEFAULT 'N/A',
+  `type` varchar(45) NOT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
+  `paid_amount` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `isFullyPaid` varchar(6) NOT NULL DEFAULT 'false',
+  `isPostPay` varchar(6) NOT NULL DEFAULT 'false',
+  `additional_comments` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `student` (
+  `id` varchar(36) NOT NULL,
+  `f_name` varchar(45) NOT NULL,
+  `l_name` varchar(45) NOT NULL,
+  `age` int(11) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `cost` (
+  `cost_code` varchar(36) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`cost_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `subject` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `cost_code` varchar(36) NOT NULL,
+  `isMandatory` varchar(6) NOT NULL DEFAULT 'false',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `cost_code_fk` FOREIGN KEY (`cost_code`) REFERENCES `cost` (`cost_code`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tutor` (
+  `id` varchar(36) NOT NULL,
+  `f_name` varchar(45) NOT NULL,
+  `l_name` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `isFulltime` varchar(6) NOT NULL DEFAULT 'true',
+  `subj_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `subj_key_fk` FOREIGN KEY (`subj_id`) REFERENCES `subject` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `payment` (
+  `id` varchar(36) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `stud_id` varchar(36) NOT NULL,
+  `datetime` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `student_id_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stud_schlrshp_link` (
+  `stud_id` varchar(36) NOT NULL,
+  `schlrshp_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`stud_id`,`schlrshp_id`),
+  CONSTRAINT `schlrshp_id_fk` FOREIGN KEY (`schlrshp_id`) REFERENCES `scholorship` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `stud_idd_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `subj_stud_link` (
+  `subj_id` varchar(36) NOT NULL,
+  `stud_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`subj_id`,`stud_id`),
+  CONSTRAINT `stud_id_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `subj_id_fk` FOREIGN KEY (`subj_id`) REFERENCES `subject` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ common/src/test/resources/sql/dml.sql
@@ -0,0 +1,52 @@
+-- Insert to student
+INSERT INTO student VALUES('studentid1','f_name1','l_name1',21,'address1');
+INSERT INTO student VALUES('studentid2','f_name2','l_name2',21,'address2');
+INSERT INTO student VALUES('studentid3','f_name3','l_name3',21,'address3');
+INSERT INTO student VALUES('studentid4','f_name4','l_name4',21,'address4');
+INSERT INTO student VALUES('studentid5','f_name5','l_name5',21,'address5');
+
+-- Insert to cost 
+INSERT INTO cost VALUES('cost_code1',1000);
+INSERT INTO cost VALUES('cost_code2',1200);
+INSERT INTO cost VALUES('cost_code3',1500);
+
+-- Insert to subject
+INSERT INTO subject VALUES('subject1','name1','cost_code1','false');
+INSERT INTO subject VALUES('subject2','name2','cost_code2','true');
+INSERT INTO subject VALUES('subject3','name3','cost_code3','false');
+INSERT INTO subject VALUES('subject4','name4','cost_code1','true');
+INSERT INTO subject VALUES('subject5','name5','cost_code2','false');
+
+-- Insert to tutor
+INSERT INTO tutor VALUES('tutorid1','f_name1','l_name1','address1', 'true', 'subject1');
+INSERT INTO tutor VALUES('tutorid2','f_name2','l_name2','address2', 'true', 'subject2');
+INSERT INTO tutor VALUES('tutorid3','f_name3','l_name3','address3', 'true', 'subject3');
+INSERT INTO tutor VALUES('tutorid4','f_name4','l_name4','address4', 'true', 'subject4');
+INSERT INTO tutor VALUES('tutorid5','f_name5','l_name5','address5', 'true', 'subject5');
+INSERT INTO tutor VALUES('tutorid6','f_name6','l_name6','address6', 'false', 'subject2');
+INSERT INTO tutor VALUES('tutorid7','f_name7','l_name7','address7', 'false', 'subject4');
+
+-- Insert to payment
+INSERT INTO payment (id, amount, type, stud_id) VALUES('payment1', 800, 'CHEQUE', 'studentid1');
+INSERT INTO payment (id, amount, type, stud_id) VALUES('payment2', 700, 'CASH', 'studentid2');
+INSERT INTO payment (id, amount, type, stud_id) VALUES('payment3', 600, 'CHEQUE', 'studentid3');
+INSERT INTO payment (id, amount, type, stud_id) VALUES('payment4', 900, 'CASH', 'studentid1');
+INSERT INTO payment (id, amount, type, stud_id) VALUES('payment5', 400, 'CHEQUE', 'studentid5');
+
+-- Insert into subj_stud_link
+INSERT INTO subj_stud_link VALUES ('subject1', 'studentid1');
+INSERT INTO subj_stud_link VALUES ('subject2', 'studentid1');
+INSERT INTO subj_stud_link VALUES ('subject3', 'studentid1');
+INSERT INTO subj_stud_link VALUES ('subject4', 'studentid1');
+INSERT INTO subj_stud_link VALUES ('subject5', 'studentid1');
+INSERT INTO subj_stud_link VALUES ('subject1', 'studentid2');
+INSERT INTO subj_stud_link VALUES ('subject1', 'studentid3');
+INSERT INTO subj_stud_link VALUES ('subject1', 'studentid4');
+INSERT INTO subj_stud_link VALUES ('subject1', 'studentid5');
+INSERT INTO subj_stud_link VALUES ('subject2', 'studentid3');
+INSERT INTO subj_stud_link VALUES ('subject2', 'studentid4');
+INSERT INTO subj_stud_link VALUES ('subject2', 'studentid5');
+INSERT INTO subj_stud_link VALUES ('subject3', 'studentid4');
+INSERT INTO subj_stud_link VALUES ('subject3', 'studentid3');
+INSERT INTO subj_stud_link VALUES ('subject3', 'studentid2');
+INSERT INTO subj_stud_link VALUES ('subject4', 'studentid4');
