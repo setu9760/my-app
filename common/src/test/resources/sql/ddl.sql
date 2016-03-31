9a5956207cd6ff5241df3af6aaddfee2319e1228
desai@BRIWS0609.sst.stp <setu_desai02@yahoo.com>
2016-03-31 10:50:09 +0100

    updated ddl script to change foreign key constraints for on delete action.

--- common/src/test/resources/sql/ddl.sql
+++ common/src/test/resources/sql/ddl.sql
@@ -19,7 +19,7 @@ CREATE TABLE `scholorship` (
   `additional_comments` varchar(100) DEFAULT NULL,
   `stud_id` varchar(36) NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `student_ids_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `student_ids_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `cost` (
@@ -31,10 +31,10 @@ CREATE TABLE `cost` (
 CREATE TABLE `subject` (
   `id` varchar(36) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `cost_code` varchar(36) NOT NULL,
+  `cost_code` varchar(36) NOT NULL DEFAULT 'N/A',
   `isMandatory` varchar(6) NOT NULL DEFAULT 'false',
   PRIMARY KEY (`id`),
-  CONSTRAINT `cost_code_fk` FOREIGN KEY (`cost_code`) REFERENCES `cost` (`cost_code`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `cost_code_fk` FOREIGN KEY (`cost_code`) REFERENCES `cost` (`cost_code`) ON DELETE SET DEFAULT ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tutor` (
@@ -43,9 +43,9 @@ CREATE TABLE `tutor` (
   `l_name` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
   `isFulltime` varchar(6) NOT NULL DEFAULT 'true',
-  `subj_id` varchar(36) NOT NULL,
+  `subj_id` varchar(36) NOT NULL DEFAULT 'N/A',
   PRIMARY KEY (`id`),
-  CONSTRAINT `subj_key_fk` FOREIGN KEY (`subj_id`) REFERENCES `subject` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `subj_key_fk` FOREIGN KEY (`subj_id`) REFERENCES `subject` (`id`) ON DELETE SET DEFAULT ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `payment` (
@@ -55,21 +55,13 @@ CREATE TABLE `payment` (
   `stud_id` varchar(36) NOT NULL,
   `datetime` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  CONSTRAINT `student_id_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `student_id_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---CREATE TABLE `stud_schlrshp_link` (
--- `stud_id` varchar(36) NOT NULL,
---`schlrshp_id` varchar(36) NOT NULL,
---  PRIMARY KEY (`stud_id`,`schlrshp_id`),
---  CONSTRAINT `schlrshp_id_fk` FOREIGN KEY (`schlrshp_id`) REFERENCES `scholorship` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
---  CONSTRAINT `stud_idd_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
---) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `subj_stud_link` (
   `subj_id` varchar(36) NOT NULL,
   `stud_id` varchar(36) NOT NULL,
   PRIMARY KEY (`subj_id`,`stud_id`),
-  CONSTRAINT `stud_id_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `subj_id_fk` FOREIGN KEY (`subj_id`) REFERENCES `subject` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `stud_id_fk` FOREIGN KEY (`stud_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `subj_id_fk` FOREIGN KEY (`subj_id`) REFERENCES `subject` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
